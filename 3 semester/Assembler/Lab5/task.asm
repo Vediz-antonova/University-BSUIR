20d3f7bed96e7161bcb7ccca72fd50aa934cace3
Vediz_antonava <antonovyss2006@gmail.com>
2024-11-18 10:38:51 +0300

    Lab5 assembler 0.2

--- 3 semester/Assembler/Lab5/task.asm
+++ 3 semester/Assembler/Lab5/task.asm
@@ -2,42 +2,130 @@
 .stack 100h
 
 .data  
-    buffer db 512 dup(0)  
+    buffer db 255 dup(0)  
     fileName db 100h dup(0)
     
-    num dw 0  
+    num dw 0            
+    lines dw 0
     lineCounter dw 0
-    res dw 11 dup(0)  
+    fileHandle dw ?
+    bytesRead dw ?
     
     msgFileName db 'Enter the file name: $'  
     msgNumber db 'Input size of word: $'  
     Enter db 0Dh, 0Ah, '$'   
-    msgEmpty db 'String is empty.$' 
-    msgCount db 'Number of lines longer than specified: $'  
+    msgEmpty db 'String is empty.$'               
+    msgLines db 'Number of lines: $'
+    msgCount db 'Number of lines longer than specified: $'
     
 .code
 start:
     mov ax, @data
-    mov ds, ax 
+    mov ds, ax  
     
-    call readSize
+    call parseCmdLineParams ; ??????? ?????????? ????????? ??????
+    cmp num, 50
+    jg notFoundBigger
+         
+    call openFile
+    call readFile
     call closeFile
     call showResult
     
-    jmp exit      
+    jmp exit   
+    
+notFoundBigger:
+    mov lineCounter, 0
+    call showResult
+    jmp exit
+    
+openFile:
+    mov dx, offset fileName
+    mov ah, 3Dh 
+    mov al, 0 
+    int 21h 
+    jc exit 
+    mov fileHandle, ax 
+    ret  
+    
+readFile proc 
+    mov ah, 3Fh 
+    mov bx, fileHandle
+    mov cx, 255 
+    lea dx, buffer 
+    int 21h 
+    jc exitFileRead
+    cmp ax, 0 
+    je exitFileRead
+    mov bytesRead, ax
+    
+    mov si, 0 
+    mov bx, 0 
+    
+nextLine: 
+    cmp si, bytesRead 
+    je readMore
+    
+    mov al, [buffer + si] 
+    cmp al, 0Dh 
+    je endOfLine 
+    cmp al, 0 
+    je endOfFile
+    inc bx 
+    inc si 
+    jmp nextLine 
+    
+endOfLine: 
+    inc lines
+    
+    cmp bx, num 
+    jle resetCounter 
+    inc lineCounter 
+    
+resetCounter: 
+    add si, 2
+    mov bx, 0
+    jmp nextLine 
+    
+readMore:
+    call readFile
+    ret
+    
+endOfFile:  
+    inc lines
+    cmp bx, num
+    jle exitFileRead
+    inc lineCounter
+
+exitFileRead: 
+    ret
+readFile endp
     
 closeFile:
     mov ah, 3Eh
+    mov bx, fileHandle
     int 21h  
     ret
     
-showResult:
+showResult:     
+    lea dx, msgLines
+    call output
+    
+    mov ax, lines   
+    lea di, lines
+    call convertToStr 
+    lea dx, di 
+    call output 
+    lea dx, Enter 
+    call output
+    
     lea dx, msgCount
     call output
     
-    lea bx, lineCounter 
+    mov ax, lineCounter   
+    lea di, lineCounter
     call convertToStr 
-    lea dx, res 
+    lea dx, di 
     call output 
     lea dx, Enter 
     call output
@@ -52,75 +140,34 @@ output:
     int 21h
     ret 
     
-readSize:      
-    lea dx, msgNumber
-    call output   
-    lea si, num
-    call input 
-    lea dx, Enter
-    call output 
-    ret 
+parseCmdLineParams proc 
+    lea di, fileName 
+    mov si, 80h 
     
-input proc   
-    mov ax, 0  
-    mov bx, 0
-    mov cx, 0 
-    mov dx, 0
-    mov bl, 0 
-    
-inputLoop: 
-    mov ah, 01h 
-    int 21h 
-    
-    cmp al, 0Dh 
-    je inputEnd
-    cmp al, '0' 
-    jb invalidInput 
-    cmp al, '9' 
-    ja invalidInput 
-    
+paramLoop: 
+    lodsb 
+    cmp al, ' ' 
+    je checkNext 
+    stosb 
+    jmp paramLoop 
+
+checkNext: 
+    cmp byte ptr [si], ' '
+    jne numberParse 
+    jmp paramLoop 
+
+numberParse: 
+    xor ax, ax 
+    lodsb 
     sub al, '0' 
-    mov dl, al  
-    mov al, bl
-    mov cl, 10
-    mul cl
-    add ax, dx
-        
-    inc cx
-    mov bx, ax 
-    
-    jmp inputLoop    
-    
-invalidInput:
-    mov ah, 0Eh 
-    mov al, 08h 
-    int 10h 
-    mov al, ' ' 
-    int 10h 
-    mov al, 08h 
-    int 10h 
-    jmp inputLoop 
+    mov num, ax 
+    ret   
+parseCmdLineParams endp
 
-inputEnd:     
-    mov [si], bx     
-    cmp cx, 0
-    je empty   
-    
-    ret  
-input endp 
-
-empty:
-    lea dx, Enter
-    call output               
-                   
-    lea dx, msgEmpty
-    call output  
-    jmp exit  
-    
 convertToStr proc
-    mov bx, 10
-    add di, 5       
-    mov byte ptr [di], '$' 
+    mov bx, 10 
+    add di, 10
+    mov byte ptr [di], '$'    
 
 convertLoop:
     dec di           

bea66bcc1701fdb5aeb040bb18e4a16abd95e1f6
Vediz_antonava <antonovyss2006@gmail.com>
2025-02-17 01:11:20 +0300

    Lab1 C#

---
+++ 4 semester/C#/Lab1/353504_Antonava_Lab1/353504_Antonava_Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using IntegralCalculator;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		IntegralCalc calculator = new IntegralCalc();
+		int numThreads = 0;
+
+		calculator.CalculationCompleted += (ticks) =>
+		{
+			Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId}: Завершен с результатом: {ticks} ticks");
+		};
+
+		calculator.ProgressChanged += (progress) =>
+		{
+			int threadId = Thread.CurrentThread.ManagedThreadId;
+
+			int totalBars = 10; 
+			int filledBars = (int)(progress / 10); 
+			string indicator = new string('=', filledBars) + new string(' ', totalBars - filledBars);
+
+			string message = $"Поток {threadId}: [{indicator}] {progress:F2}%\n";
+			Console.SetCursorPosition(0, Console.CursorTop);
+			Console.Write(message);
+		};
+
+		for (int i = 0; i < 5; i++)
+		{
+			Thread calculationThread = new Thread(() =>
+			{
+				double result = calculator.CalculateIntegral();
+				numThreads++;
+			});
+
+			calculationThread.Start();
+		}
+	}
+}
---
+++ 4 semester/C#/Lab1/353504_Antonava_Lab1/IntegralCalculator/IntegralCalc.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace IntegralCalculator
+{
+	public class IntegralCalc
+	{
+		private static SemaphoreSlim semaphore = new SemaphoreSlim(2);
+
+		public event Action<double> CalculationCompleted;
+		public event Action<double> ProgressChanged;
+		public double CalculateIntegral()
+		{
+			semaphore.Wait();
+
+			try
+			{
+				double integral = 0.0;
+				double x = 0.0;
+				double step = 0.00000001;
+
+				double totalSteps = 1.0 / step;
+
+				Stopwatch stopwatch = new Stopwatch();
+				stopwatch.Start();
+
+				while (x < 1.0)
+				{
+					integral += Math.Sin(x) * step;
+					x += step;
+
+					double progress = (x / 1.0) * 100;
+					ProgressChanged?.Invoke(progress);
+
+					for (int i = 0; i < 100000; i++)
+					{
+						double temp = 2 * 5;
+					}
+				}
+
+				stopwatch.Stop();
+				CalculationCompleted?.Invoke(stopwatch.Elapsed.Ticks);
+
+				return integral;
+			}
+			finally
+			{
+				semaphore.Release();
+			}
+		}
+	}
+}
